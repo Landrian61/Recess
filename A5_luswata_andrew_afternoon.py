b13c9f63b1528ab24152b42d5a84934aa8e126f0
landrian61 <luswataandrew190@gmail.com>
2023-06-30 18:00:59 +0300

    Reciept GUI

---
+++ A5_luswata_andrew_afternoon.py
@@ -0,0 +1,107 @@
+# Exceptions
+# Exceptions are a way to handle errors or exceptional conditions that may occur during the execution of a program.
+#  When an exceptional situation arises, an exception object is raised, and the normal flow of the program is interrupted.
+#  If the exception is not handled, it will cause the program to terminate and display an error message.
+
+def divide_numbers():
+    try:
+        a = input("Enter first number :")
+        b = input("Enter second number :")
+        result = a / b
+        print(f"The result of division is: {result}")
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+    except TypeError:
+        print("Error: Invalid operand types for division.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        print("Division operation completed.")
+
+
+# Example usage
+divide_numbers()
+
+# Custom Exceptions
+
+
+class InvalidInputException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+def calculate_square_root(num):
+    if num < 0:
+        raise InvalidInputException("Input must be a non-negative number.")
+    return num ** 0.5
+
+
+try:
+    number = float(input("Enter a number: "))
+    result = calculate_square_root(number)
+    print("Square root:", result)
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+except InvalidInputException as e:
+    print("Error:", e.message)
+
+
+# File handling
+# File handling in Python is the process of working with files, which can be used to read data from or write data to files on a computer.
+# 1. Opening a file:
+#    To work with a file, you first need to open it using the open() function.
+#    The open() function takes two parameters: the file name (or path) and the mode in which you want to open the file.
+#    The mode can be "r" for reading, "w" for writing (overwriting existing file), "a" for appending (adding data to the end of an existing file),
+#    or "x" for creating a new file.
+# 2. Reading from a file:
+#    Once a file is opened in read mode, you can read its contents using various methods.
+#    The most common method is read(), which reads the entire file as a string.
+#    You can also read the file line by line using the readline() method or get a list of all lines using the readlines() method.
+# 3. Writing to a file:
+#    When a file is opened in write or append mode, you can write data to it using the write() method.
+#    If you open a file in write mode ("w"), it will overwrite the existing content.
+#    To avoid overwriting, you can open the file in append mode ("a"), which allows you to add data to
+#    the end of the file without removing the existing content.
+# 4. Closing the file
+#     After you finish working with a file, it's important to close it using the close() method.
+#     This releases the system resources associated with the file.
+#     However, a more recommended approach is to use the with statement, which automatically takes care of closing the file for you.
+#     The with statement ensures that the file is properly closed, even if an exception occurs within the block.
+
+# # Example usage
+# # Opening a file in write mode and writing data to it
+file = open("example.txt", "w")
+file.write("Hello, World!\n")
+file.write("This is a demo file.\n")
+file.close()
+
+# # Opening the same file in read mode and reading its contents
+file = open("example.txt", "r")
+content = file.read()
+print("File Contents:\n", content)
+file.close()
+
+# # Opening the same file in read mode and reading line by line
+file = open("example.txt", "r")
+line = file.readline()
+print("First line:", line)
+lines = file.readlines()
+print("Remaining lines:", lines)
+file.close()
+
+# # Opening the same file in append mode and appending more data to it
+file = open("example.txt", "a")
+file.write("Appending new line.\n")
+file.close()
+
+# # Opening the same file in read mode again and printing its updated contents
+file = open("example.txt", "r")
+content = file.read()
+print("Updated File Contents:\n", content)
+file.close()
+
+# # Using the with statement to automatically close the file
+with open("example.txt", "r") as file:
+    content = file.read()
+    print("File Contents using with statement:\n", content)
--- A5_luswata_andrew_morning.py
+++ A5_luswata_andrew_morning.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
+from abc import ABC, abstractmethod
 from ttkthemes import ThemedStyle
 from tkinter.ttk import Button, Entry, Style
 
 
-class ReceiptPrinter:
+class ReceiptPrinter(ABC):
     def __init__(self, master):
         self.master = master
         master.title("Receipt Printer")
@@ -18,29 +19,16 @@ class ReceiptPrinter:
         self.custom_style.configure("TEntry", padding=5)
 
         # Create labels and entry fields
-        self.label_name = tk.Label(master, text="Name:")
-        self.label_name.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
-        self.entry_name = Entry(master, style="TEntry")
-        self.entry_name.grid(row=0, column=1, padx=10, pady=10)
-
-        self.label_items = tk.Label(master, text="Items (comma-separated):")
-        self.label_items.grid(row=1, column=0, sticky=tk.W, padx=10, pady=10)
-        self.entry_items = Entry(master, style="TEntry")
-        self.entry_items.grid(row=1, column=1, padx=10, pady=10)
-
-        self.label_total = tk.Label(master, text="Total:")
-        self.label_total.grid(row=2, column=0, sticky=tk.W, padx=10, pady=10)
-        self.entry_total = Entry(master, style="TEntry")
-        self.entry_total.grid(row=2, column=1, padx=10, pady=10)
+        self.create_widgets()
 
         # Create print button
         self.print_button = Button(
             master, text="Print Receipt", command=self.print_receipt)
-        self.print_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+        self.print_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
         # Center the window on the screen
         window_width = 400
-        window_height = 250
+        window_height = 300
         screen_width = master.winfo_screenwidth()
         screen_height = master.winfo_screenheight()
         x = int((screen_width / 2) - (window_width / 2))
@@ -49,6 +37,37 @@ class ReceiptPrinter:
 
         # Disable resizing of the window
         master.resizable(False, False)
+
+    @abstractmethod
+    def create_widgets(self):
+        pass
+
+    @abstractmethod
+    def print_receipt(self):
+        pass
+
+
+class AdvancedReceiptPrinter(ReceiptPrinter):
+    def __init__(self, master):
+        super().__init__(master)
+
+    def create_widgets(self):
+        # Create labels and entry fields
+        self.label_name = tk.Label(self.master, text="Name:")
+        self.label_name.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
+        self.entry_name = Entry(self.master, style="TEntry")
+        self.entry_name.grid(row=0, column=1, padx=10, pady=10)
+
+        self.label_items = tk.Label(
+            self.master, text="Items (comma-separated):")
+        self.label_items.grid(row=1, column=0, sticky=tk.W, padx=10, pady=10)
+        self.entry_items = Entry(self.master, style="TEntry")
+        self.entry_items.grid(row=1, column=1, padx=10, pady=10)
+
+        self.label_total = tk.Label(self.master, text="Total:")
+        self.label_total.grid(row=2, column=0, sticky=tk.W, padx=10, pady=10)
+        self.entry_total = Entry(self.master, style="TEntry")
+        self.entry_total.grid(row=2, column=1, padx=10, pady=10)
 
     def print_receipt(self):
         name = self.entry_name.get()
@@ -68,11 +87,24 @@ class ReceiptPrinter:
         self.entry_total.delete(0, tk.END)
 
 
+class OverloadedReceiptPrinter(AdvancedReceiptPrinter):
+    def __init__(self, master):
+        super().__init__(master)
+
+    def create_widgets(self):
+        super().create_widgets()
+
+        self.label_address = tk.Label(self.master, text="Address:")
+        self.label_address.grid(row=3, column=0, sticky=tk.W, padx=10, pady=10)
+        self.entry_address = Entry(self.master, style="TEntry")
+        self.entry_address.grid(row=3, column=1, padx=10, pady=10)
+
+
 # Create the main window
 root = tk.Tk()
 
-# Create an instance of the ReceiptPrinter class
-receipt_printer = ReceiptPrinter(root)
+# Create an instance of the OverloadedReceiptPrinter class
+receipt_printer = OverloadedReceiptPrinter(root)
 
 # Start the GUI event loop
 root.mainloop()
